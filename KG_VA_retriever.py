2c4f2871268108b806d31b2fbb1a3a4b4d8762d7
bpusch03 <benjaminpusch03@gmail.com>
2023-08-13 16:17:57 -0400

    fixed bug in kg_va_retriever

--- KG_VA_retriever.py
+++ KG_VA_retriever.py
@@ -1,37 +1,37 @@
-try:
-    from llama_index import QueryBundle
-    from llama_index.indices.query.schema import QueryBundle
-    from llama_index.schema import NodeWithScore
-    from llama_index.retrievers import BaseRetriever
-    from typing import List
+from llama_index import QueryBundle
+from llama_index.indices.query.schema import QueryBundle
+from llama_index.schema import NodeWithScore
+from llama_index.retrievers import BaseRetriever
+from typing import List
 
-    class KG_VA_retreiver(BaseRetriever):
-        def __init__(self, vector_retriever, kg_retriever, mode='AND'):
-            self._vector_retriever = vector_retriever
-            self._kg_retriever = kg_retriever
-            if mode not in ("AND", "OR"):
-                raise ValueError("Invalid mode.")
-            self._mode = mode
+class KG_VA_retreiver(BaseRetriever):
+    def __init__(self, vector_retriever, kg_retriever, mode='AND'):
+        self._vector_retriever = vector_retriever
+        self._kg_retriever = kg_retriever
+        if mode not in ("AND", "OR"):
+            raise ValueError("Invalid mode.")
+        self._mode = mode
 
-        def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
-            vector_nodes = self._vector_retriever.retrieve(query_bundle)
-            kg_nodes = self._kg_retriever.retrieve(query_bundle)
+    def _retrieve(self, query_bundle: QueryBundle) -> List[NodeWithScore]:
+        vector_nodes = self._vector_retriever.retrieve(query_bundle)
+        kg_nodes = self._kg_retriever.retrieve(query_bundle)
 
-            vector_ids = {n.node.node_id for n in vector_nodes}
-            kg_ids = {n.node.node_id for n in kg_nodes}
+        vector_ids = {n.node.node_id for n in vector_nodes}
+        kg_ids = {n.node.node_id for n in kg_nodes}
 
-            combined_dict = {n.node.node_id: n for n in vector_nodes}
-            combined_dict.update({n.node.node_id: n for n in kg_nodes})
+        combined_dict = {n.node.node_id: n for n in vector_nodes}
+        combined_dict.update({n.node.node_id: n for n in kg_nodes})
 
-            if self._mode == "AND":
-                retrieve_ids = vector_ids.intersection(kg_ids)
-            else:
-                retrieve_ids = vector_ids.union(kg_ids)
-            
-            retrieve_nodes = [combined_dict[rid] for rid in retrieve_ids]
-            return retrieve_nodes
+        if self._mode == "AND":
+            retrieve_ids = vector_ids.intersection(kg_ids)
+        else:
+            retrieve_ids = vector_ids.union(kg_ids)
         
-    if __name__ == 'main':
+        retrieve_nodes = [combined_dict[rid] for rid in retrieve_ids]
+        return retrieve_nodes
+    
+if __name__ == 'main':
+    try:
         #build service context
         #build service context for querying
         from llama_index.llms import HuggingFaceLLM
@@ -134,5 +134,5 @@ try:
         response = query_engine.query("What McIntire student wrote for Key&Peele?")
 
         print(response)
-except Exception as e:
-    print(e)
+    except Exception as e:
+        print(e)
